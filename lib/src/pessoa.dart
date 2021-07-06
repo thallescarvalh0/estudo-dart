86e9b5508f43421e2aa462b745ebcbeccfe90953
Thalles Carvalho <thallescarvalhoferreira@gmail.com>
2021-07-05 21:28:01 -0300

    remoção comentário

--- lib/src/pessoa.dart
+++ lib/src/pessoa.dart
@@ -4,8 +4,6 @@ class Pessoa extends Humano{
   
   String? nome;
   int? idade;
-  
-  //final String otherNome = 'Thalles'; //final só permite atribuir valor uma unica vez
 
   Pessoa({this.nome, this.idade, peso, altura}) :super(peso: peso, altura: altura);
 
