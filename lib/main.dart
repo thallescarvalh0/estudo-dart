d07fbd6793177390326be372588294381513572f
Thalles Carvalho <thallescarvalhoferreira@gmail.com>
2021-07-05 21:25:03 -0300

    master

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+
+import 'package:poo_dart/src/pessoa.dart';
+
+void main(List<String> arguments) {
+  var pessoa = Pessoa(nome: 'thalles', idade: 25, peso: 45);
+
+  print(pessoa.peso);
+}
---
+++ lib/src/pessoa.dart
@@ -0,0 +1,12 @@
+import 'package:poo_dart/src/humano.dart';
+
+class Pessoa extends Humano{
+  
+  String? nome;
+  int? idade;
+  
+  //final String otherNome = 'Thalles'; //final só permite atribuir valor uma unica vez
+
+  Pessoa({this.nome, this.idade, peso, altura}) :super(peso: peso, altura: altura);
+
+}
